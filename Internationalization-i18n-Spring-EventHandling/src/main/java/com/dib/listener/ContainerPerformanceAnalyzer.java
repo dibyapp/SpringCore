b8fa48f17808fcb3beea15029052e390f9a6c830
ayesit <ayesit.com@gmail.com>
2020-04-28 20:15:43 +0530

    Spring

---
+++ Internationalization-i18n-Spring-EventHandling/src/main/java/com/dib/listener/ContainerPerformanceAnalyzer.java
@@ -0,0 +1,28 @@
+package com.dib.listener;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+public class ContainerPerformanceAnalyzer implements ApplicationListener {
+
+	private long start,end;
+	
+	public ContainerPerformanceAnalyzer() {
+		System.out.println("ContainerPerformanceAnalyzer.ContainerPerformanceAnalyzer()");
+	}
+	
+	@Override
+	public void onApplicationEvent(ApplicationEvent event){
+		if(event.toString().contains("Refreshed"))
+			start=System.currentTimeMillis();
+		else if(event.toString().contains("Closed")) {
+			end=System.currentTimeMillis();
+			System.out.println("Container is in running mode for :"+(end-start)+"ms");
+		}
+		
+		
+	}
+
+	
+
+}
