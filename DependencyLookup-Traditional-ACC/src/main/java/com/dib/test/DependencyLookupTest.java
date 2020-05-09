d811f9c660833e2528c0d18dad2fe9c9929e3ef2
ayesit <ayesit.com@gmail.com>
2020-05-09 21:07:38 +0530

    Spring

---
+++ DependencyLookup-Traditional-ACC/src/main/java/com/dib/test/DependencyLookupTest.java
@@ -0,0 +1,30 @@
+package com.dib.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.dib.beans.MobilePhone;
+
+public class DependencyLookupTest {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ctx=null;
+		MobilePhone phone=null;
+	//create IOC container
+		System.out.println("1st IOC container");
+		 ctx=new ClassPathXmlApplicationContext("com/dib/configurations/applicationContext.xml");
+		 //get Target bean class object
+		 phone=ctx.getBean("phone",MobilePhone.class);
+		 System.out.println(".........................................");
+		 //invoke methods
+		 phone.batteryCharging();
+		 phone.entertainment();
+		 phone.calls();
+		 phone.gaming();
+		 phone.shopping();
+		
+		
+	}
+
+}
